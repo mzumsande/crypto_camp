5ac650c61fa80c113b2ceedba16cfe2891089d6c
Martin Zumsande <mzumsande@gmail.com>
2025-06-27 14:52:12 -0400

    exercise 3

--- week1/fast_exp.py
+++ week1/fast_exp.py
@@ -1,4 +1,10 @@
 
+def inv_mult(a, p):
+    #could check if p is really prime
+    # apply fermats little theorem
+    return fast_exp(a, p - 2, p)
+
+
 def fast_exp(g, exp, mod):
     res = 1
     gpow = g % mod
@@ -9,6 +15,12 @@ def fast_exp(g, exp, mod):
         exp >>= 1
     return res
 
+
+
+
 assert fast_exp(3, 218, 1000) == 489
 assert fast_exp(2, 10, 1000) == pow(2, 10, 1000)
 assert fast_exp(7, 256, 13) == pow(7, 256, 13)
+
+assert(inv_mult(22, 211)) == 48
+assert(inv_mult(60, 101)) == 32
